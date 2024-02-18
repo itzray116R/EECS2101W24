c4eee4f53dcc037d4c3636203068088f70958bfb
itzray116R <rayhaanyaser@gmail.com>
2024-02-18 18:34:23 -0500

    Add Lab04

--- Lab4/src/lab4/YorkQueueUsingStacks.java
+++ Lab4/src/lab4/YorkQueueUsingStacks.java
@@ -37,62 +37,100 @@ public class YorkQueueUsingStacks<E> implements Queue<E> {
 	 * Add any other private data members or methods that are necessary 
 	 * to manage the YorkArrayStack stored
 	 */
-	
-	
-	
+	private Stack<E> stack1;
+	private Stack<E> stack2;	
 
 	public YorkQueueUsingStacks(){
-		// TODO: Your implementation of this method starts here
+		this.stack1 = new YorkArrayStack<>();
+		this.stack2 = new YorkArrayStack<>();
 	}
 	
 	
 	@Override
 	public int size() {
-		// TODO: Your implementation of this method starts here
-		return 0;
+		return  stack1.size() + stack2.size();
 	
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO: Your implementation of this method starts here
-		return false;
+		return  stack1.isEmpty() && stack2.isEmpty();
 		
 	}
 
 	@Override
 	public void clear() {
-		// TODO: Your implementation of this method starts here
-		
-		
+		stack1.clear();
+		stack2.clear();
 	}
 
 	@Override
 	public void enqueue(E e) {
-		// TODO: Your implementation of this method starts here
+		stack1.push(e);
+	}
 		
-	}
 
 	@Override
-	public E first() throws NoSuchElementException {
-		// TODO: Your implementation of this method starts here
-		return null;
-
-	}
+    public E first() throws NoSuchElementException {
+        if (isEmpty()) throw new NoSuchElementException("The queue is empty");
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.top(); // Return the front tmp without removing it
+    }
 
 	@Override
-	public E dequeue() throws NoSuchElementException {
-		// TODO: Your implementation of this method starts here
-		return null;	
-	}
+    public E dequeue() throws NoSuchElementException {
+        if (isEmpty()) throw new NoSuchElementException("Queue is Empty");
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop(); // Remove and return the front tmp
+    }
 
 	@Override
 	public boolean contains(E e) throws NullPointerException {
-		// TODO: Your implementation of this method starts here
+		// Check if the tmp is present in stack2
+		Stack<E> tempStack = new YorkArrayStack<>();
+		while (!stack2.isEmpty()) {
+			E tmp = stack2.pop();
+			tempStack.push(tmp);
+			if (Objects.equals(e, tmp)) {
+
+				while (!tempStack.isEmpty()) {
+					stack2.push(tempStack.pop());
+				}
+				return true;
+			}
+		}
+
+		while (!tempStack.isEmpty()) {
+			stack2.push(tempStack.pop());
+		}
+
+		while (!stack1.isEmpty()) {
+			E tmp = stack1.pop();
+			tempStack.push(tmp);
+			if (Objects.equals(e, tmp)) {
+
+				while (!tempStack.isEmpty()) {
+					stack1.push(tempStack.pop());
+				}
+				return true;
+			}
+		}
+
+		while (!tempStack.isEmpty()) {
+			stack1.push(tempStack.pop());
+		}
+
 		return false;
-	
-		
 	}
+
 	
 	
 	/**
@@ -101,14 +139,34 @@ public class YorkQueueUsingStacks<E> implements Queue<E> {
 	 * example "[30, 110, -110, -2, 1322]"
 	 * 
 	 * remember that you should displays the contents of the queue in insertion order (FIFO), so the
-	 * most-recently inserted element should be the last element
+	 * most-recently inserted tmp should be the last tmp
 	 * 
 	 */
 	@Override
 	public String toString() {
-		// TODO: Your implementation of this method starts here
-		return "";
-	}
-	
 
+		StringBuilder sb = new StringBuilder("[");
+		YorkArrayStack<E> tempStack = new YorkArrayStack<>();
+
+		while (!stack2.isEmpty()) {
+			tempStack.push(stack2.pop());
+		}
+
+		sb.append(tempStack.toString().substring(1, tempStack.toString().length() - 1));
+
+		while (!tempStack.isEmpty()) {
+			stack2.push(tempStack.pop());
+		}
+
+		if (!stack1.isEmpty()) {
+			if (!stack2.isEmpty()) {
+				sb.append(", ");
+			}
+			sb.append(stack1.toString().substring(1, stack1.toString().length() - 1));
+		}
+
+		sb.append("]");
+
+		return sb.toString();
+	}
 }
